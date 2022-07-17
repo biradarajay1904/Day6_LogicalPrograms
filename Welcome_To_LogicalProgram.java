6b236845c11b35fe4e2996153fd153fa95eddfa6
biradarajay1904 <biradarajay04@gmail.com>
2022-07-17 22:37:39 +0530

    moodified: fibonacci series

--- Welcome_To_LogicalProgram.java
+++ Welcome_To_LogicalProgram.java
@@ -5,6 +5,22 @@ public class Welcome_To_LogicalProgram {
 	public static void main(String[] args) {
 	
 System.out.println("Welcome to logical program");
-	}
+int n = 10, firstTerm = 0, secondTerm = 1;
+System.out.println("Fibonacci Series till " + n + " terms:");
+
+for (int i = 1; i <= n; ++i) {
+  System.out.print(firstTerm + ", ");
+
+  // compute the next term
+  int nextTerm = firstTerm + secondTerm;
+  firstTerm = secondTerm;
+  secondTerm = nextTerm;
+
 
 }
+}
+
+
+	}
+
+
